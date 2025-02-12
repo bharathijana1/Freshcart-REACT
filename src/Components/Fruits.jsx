dba87ffd61f4594c080f97615d8948bead6dd84c
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-12 11:38:11 +0530

    Update Fruits.jsx

--- src/Components/Fruits.jsx
+++ src/Components/Fruits.jsx
@@ -10,7 +10,6 @@ import StraberryImg from '../Components/img/fruits/straberry.jpg';
 import GuavaImg from '../Components/img/fruits/guava.jpg';
 import WatermelonImg from '../Components/img/fruits/watermelon.jpg';
 import MangoImg from '../Components/img/fruits/mango.jpg';
-import FruitsiconImg from '/components/img/fruitsicon.png';
 
 const Fruits = () => {
   const { addItem } = useCart();  
@@ -108,7 +107,7 @@ const Fruits = () => {
   return (
     <div>
       <div className='flex justify-start md:justify-center gap-3 p-2 m-2'>
-        <img src={FruitsiconImg} alt="fruit icon" width="100px" height="100px" />
+        <img src="..src/components/img/fruitsicon.png" alt="fruit icon" width="100px" height="100px" />
         <h1 className='text-center font-bold flex justify-center items-center text-3xl'>Fruits</h1>
       </div>
 
