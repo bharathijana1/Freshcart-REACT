455f2a285e849584c7c53ebe6caeb6203d2d3b05
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-12 11:08:10 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 import { HiMenu } from 'react-icons/hi'; 
 import { useCart } from 'react-use-cart';
+import CartlogoImg from '../Components/img/shopping-cart.png';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +20,7 @@ const NavBar = () => {
     <div className='flex justify-between p-3 text-white' style={{backgroundColor:"#3B1C32"}}>
       <Link to="/">
       <div className='text-2xl md:text-3xl font-bold tracking-wide flex justify-center items-center gap-3' onClick={()=>navigate("/")}>
-        <img src="../src/Components/img/shopping-cart.png" alt="logo" width="50px" height="50px" />
+        <img src={CartlogoImg} alt="logo" width="50px" height="50px" />
         <h1>FreshCart</h1>
       </div>
       </Link>
@@ -61,7 +62,7 @@ const NavBar = () => {
           <Link to="/carts">
           <h2 className='text-xl p-1 '>
             <div className='flex flex-row justify-center items-center'>
-            <img src="../src/Components/img/basket.png" alt="basket" width="40px" height="50px" />
+            <img src={CartlogoImg} alt="basket" width="40px" height="50px" />
             <div className='flex justify-center items-center '>
             <span className='absolute left-0 bottom-0 w-full h-0.5 bg-white scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left'></span>
             <span className='text-pink-900 px-2 mx-2 bg-white rounded-full text-xl'>{totalItems}</span>
